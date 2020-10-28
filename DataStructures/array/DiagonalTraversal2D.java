2106aac1c06eb263f3e5e56eaf7b17e0a56ce53c
Gaganpreet Singh <gagan20683@gmail.com>
2020-10-28 23:28:27 +0530

    Diagonal Traversal of an 2D array

---
+++ DataStructures/array/DiagonalTraversal2D.java
@@ -0,0 +1,55 @@
+package array;
+
+import java.util.Scanner;
+//diagonal traversal of an 2D array
+public class DiagonalTraversal2D {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// input array
+		Scanner scn = new Scanner(System.in);
+		
+		int row = scn.nextInt();
+		int col = scn.nextInt();
+		
+		int arr [][] = new int[row][col];
+		
+		for(int i=0; i<row;i++)
+		{
+			for(int j=0;j<col;j++)
+			{
+				arr[i][j] = scn.nextInt();
+			}
+		}
+//print array diagonally 		
+    	for(int k=0;k<= row -1;k++)
+		{
+			int i =k;
+			int j =0;
+			while(i>=0)
+			{
+				System.out.print(arr[i][j] + " ");
+				i =i-1;
+				j=j+1;
+				
+			}
+			System.out.println();
+		}
+		
+		for(int k=1;k<=col-1;k++)
+		{
+			int i = row -1;
+			int j =k;
+			
+			while(j <= col-1)
+			{
+				System.out.print(arr[i][j] + " ");
+				i =i-1;
+				j= j+1;
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
